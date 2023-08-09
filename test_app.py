d25206596f5a977a26ba67b70b3c6a9415679903
tinghong97 <121005783+tinghong97@users.noreply.github.com>
2023-08-10 03:38:24 +0800

    Add files via upload

---
+++ test_app.py
@@ -0,0 +1,22 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_add_todo_item(client):
+    response = client.post('/add', json={'task': 'Buy groceries'})
+    assert response.status_code == 201
+
+def test_list_todo_items(client):
+    response = client.get('/list')
+    assert response.status_code == 200
+    assert 'todo_list' in response.json
+
+# Add more test cases for other API endpoints
+
+if __name__ == '__main__':
+    pytest.main()
